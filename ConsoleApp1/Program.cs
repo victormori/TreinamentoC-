13028dc342738203dce842137f00583af9913855
victormori <victordaab@gmail.com>
2026-03-18 16:01:37 -0300

    informaçoes sobre operadores

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
-﻿string primeiroNome = "Ana";
-string segundoNome = "Beatriz";
-Console.WriteLine($"Meu nome é {primeiroNome} {segundoNome}");
+﻿int value = 1;
+
+// ++ incrementa 1 na variaável value 
+value++;
+Console.WriteLine($"incremento: {value}");
+
+// += soma value(1) com 5 
+value += 5;
+Console.WriteLine($"Incremento: {value}");
+
+// -+ decrementa 5 do value
+value -= 5;
+Console.WriteLine($"decremento: {value} ");
+
+// -- decrementa 1 do value
+value--;
+Console.WriteLine($"decremento: {value}");
+
